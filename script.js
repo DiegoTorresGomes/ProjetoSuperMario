2e16d33dbe170be589a582e010b8f2534ca82b73
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-08-16 19:43:55 -0300

    ProjetoMario

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
 const gameOver = document.querySelector('.gameOver');
 const botao = document.getElementById('play');
-
+const floorA = document.querySelector('.floor-1');
+const floorB = document.querySelector('.floor-2');
+const floorC = document.querySelector('.floor-3');
 botao.addEventListener("click", () => {
     location.reload()
 });
@@ -24,7 +26,7 @@ const loop = setInterval(() => {
    const pipePosition = pipe.offsetLeft;
    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
    const cloudsPosition = +window.getComputedStyle(clouds).left.replace('px', '');
-   
+   const floorPosition = +window.getComputedStyle(floorA, floorB, floorC).left.replace('px', '');
 
 
    console.log(marioPosition)
@@ -39,12 +41,26 @@ const loop = setInterval(() => {
 
     clouds.style.animation = 'none';
     clouds.style.left = `${cloudsPosition}px`;
+
+    floorA.style.animation = 'none';
+    floorA.style.left = `${floorPosition}px`;
+
+    floorB.style.animation = 'none';
+    floorB.style.left = `${floorPosition}px`;
    
+    floorC.style.animation = 'none'; 
+    floorC.style.left = `${floorPosition}px`;
+
+    
+   
+
 
     mario.src = './Assets/Imagens do projeto/game-over.png';
     mario.style.width = '75px';
     mario.style.marginLeft = '50px';
     gameOver.style.display = 'block';
+    floorA.style.width = '100%';
+    
 
     clearInterval(loop);
 
